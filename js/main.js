e95cc9571cc13bc2e77e81b3d0a8e2f2728389cb
choroy <roycho73@gmail.com>
2016-10-22 13:19:34 +0900

    jquery zoom js 플러그인 생성

--- js/main.js
+++ js/main.js
@@ -1,36 +1,6 @@
 $(function () {
+    var zoom = new Zoom('.zoom', 1.4, 400);
 
-    var $zoom = $('.zoom li'),
-         width = $zoom.width(),
-         zoom = 1.4,
-         duration = 400;
-
-    $zoom.on({
-        mouseenter: function () {
-            //that, this , self 라고 쓰기도 한다.
-            var $self = $(this),
-                 height =  $self.height();
-
-            $self
-                .find('.img')
-                .stop()
-                .animate({
-                    width:  width * zoom,
-                    marginLeft:-( width * zoom -  width) / 2,
-                    marginTop:-(height * zoom - height) / 2
-                 }, duration);
-        },
-        mouseleave: function () {
-
-          $(this)
-              .find('.img')
-              .stop()
-              .animate({
-                  width:  width,
-                  marginLeft: 0,
-                  marginTop:0
-              },duration);
-        }
-    });
+    
 
 });
